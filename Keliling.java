fbc7e78f0bc9102e0dd3ead4cbf4067c18e3a853
Farizfa <86603598+Farizfa@users.noreply.github.com>
2023-03-09 13:45:52 +0700

    Add files via upload
    Menambahkan tugas 1 (Keliling. java; Luas.java; Volume.java)

---
+++ Keliling.java
@@ -0,0 +1,9 @@
+public class Keliling {
+    public static void main(String[] args) {
+        double d = 10;
+        // double phi = 3.14;
+        double kell = 3.14 * d;
+
+        System.out.println("Keliling : 3,14 * " + d + " = " + kell);
+    }
+}
---
+++ Luas.java
@@ -0,0 +1,9 @@
+public class Luas {
+    public static void main(String[] args) {
+        double a = 6, t = 8;
+        double hitung = (a * t) / 2;
+
+        System.out.println("Hasil Hitung Luas Segitiga dengan alas " + a + " dan tinggi " + t + " adalah " + hitung);
+    }
+
+}
