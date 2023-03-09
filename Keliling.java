1d425665689c48d9ec182a3f1d69d86365c1b62a
Farizfa <86603598+Farizfa@users.noreply.github.com>
2023-03-09 13:51:10 +0700

    Delete Keliling.java

--- Keliling.java
+++
@@ -1,9 +0,0 @@
-public class Keliling {
-    public static void main(String[] args) {
-        double d = 10;
-        // double phi = 3.14;
-        double kell = 3.14 * d;
-
-        System.out.println("Keliling : 3,14 * " + d + " = " + kell);
-    }
-}
